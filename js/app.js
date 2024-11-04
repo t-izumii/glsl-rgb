24aa5090a5c1a67adf40a51ddbb836df6b19a121
izumi tetsuya <t.izumi@andco-web.jp>
2024-11-04 20:14:08 +0900

    update

--- js/app.js
+++ js/app.js
@@ -136,68 +136,10 @@ class MeshItem {
         this.getDimensions();
         this.mesh.position.set(this.offset.x , this.offset.y , 0);
         this.mesh.scale.set(this.sizes.x , this.sizes.y);
-        this.uniforms.uOffset.value.set(this.offset.x * 0.0, -(target- current) * 0.0003 )
+        this.uniforms.uOffset.value.set(this.offset.x * 0.0, -(target- current) * 0.0006 )
     }
 
 }
-
-// class MeshItem{
-//     // Pass in the scene as we will be adding meshes to this scene.
-//     constructor(element, scene){
-//         this.element = element;
-//         this.scene = scene;
-//         this.offset = new THREE.Vector2(0,0); // Positions of mesh on screen. Will be updated below.
-//         this.sizes = new THREE.Vector2(0,0); //Size of mesh on screen. Will be updated below.
-//         this.createMesh();
-//     }
-
-//     getDimensions(){
-//         const {width, height, top, left} = this.element.getBoundingClientRect();
-//         this.sizes.set(width, height);
-//         this.offset.set(left - window.innerWidth / 2 + width / 2, -top + window.innerHeight / 2 - height / 2); 
-//     }
-
-//     createMesh(){
-//         this.geometry = new THREE.PlaneGeometry(1,1,100,100);
-//         this.imageTexture = new THREE.TextureLoader().load(this.element.src);
-//         this.uniforms = {
-//             uTexture: {
-//                 //texture data
-//                 value: this.imageTexture
-//               },
-//               uOffset: {
-//                 //distortion strength
-//                 value: new THREE.Vector2(0.0, 0.0)
-//               },
-//               uAlpha: {
-//                 //opacity
-//                 value: 1.
-//               }
-//         };
-//         this.material = new THREE.ShaderMaterial({
-//             uniforms: this.uniforms,
-//             vertexShader: vertexShader,
-//             fragmentShader: fragmentShader,
-//             transparent: true,
-//             // wireframe: true,
-//             side: THREE.DoubleSide
-//         })
-//         this.mesh = new THREE.Mesh( this.geometry, this.material );
-//         this.getDimensions(); // set offsetand sizes for placement on the scene
-//         this.mesh.position.set(this.offset.x, this.offset.y, 0);
-// 		this.mesh.scale.set(this.sizes.x, this.sizes.y, 1);
-
-//         this.scene.add( this.mesh );
-//     }
-
-//     render(){
-//         // this function is repeatidly called for each instance in the aboce 
-//         this.getDimensions();
-//         this.mesh.position.set(this.offset.x, this.offset.y, 0)
-// 		this.mesh.scale.set(this.sizes.x, this.sizes.y, 1)
-//         this.uniforms.uOffset.value.set(this.offset.x * 0.0, -(target- current) * 0.0003 )
-//     }
-// }
 
 
 
